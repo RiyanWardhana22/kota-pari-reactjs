c2d3a507eca3e30103835bedff31385a7cf83de7
riyanwardhana <riyanwardhana2@gmail.com>
2026-07-28 15:27:57 +0700

    FIX: Map Dashboard

--- src/data/historicalSites.js
+++ src/data/historicalSites.js
@@ -83,39 +83,6 @@ export const historicalSites = [
     significanceLevel: 4,
     imageUrl: "/assets/images/galeri-4.jpeg",
   },
-  // {
-  //   id: "site-03",
-  //   name: "Makam Datuk Keramat",
-  //   category: "sejarah",
-  //   coordinates: [3.662145, 98.948512],
-  //   shortDescription:
-  //     "Tempat sakral yang dihormati warga setempat, diyakini sebagai makam tokoh penyebar agama atau pendiri awal permukiman pesisir Kota Pari.",
-  //   yearEstablished: "Pra-1900",
-  //   significanceLevel: 5,
-  //   imageUrl: "/assets/images/makam-keramat.jpg",
-  // },
-  // {
-  //   id: "site-04",
-  //   name: "Dermaga Tradisional Nelayan",
-  //   category: "maritim",
-  //   coordinates: [3.66621, 98.94715],
-  //   shortDescription:
-  //     "Urat nadi perekonomian warga. Tempat bersandarnya kapal-kapal nelayan tradisional yang membawa hasil tangkapan segar dari laut lepas.",
-  //   yearEstablished: "1920-an",
-  //   significanceLevel: 5,
-  //   imageUrl: "/assets/images/dermaga.jpg",
-  // },
-  // {
-  //   id: "site-05",
-  //   name: "Masjid Tua Pesisir",
-  //   category: "sejarah",
-  //   coordinates: [3.65834, 98.95011],
-  //   shortDescription:
-  //     "Pusat penyebaran agama Islam dan kegiatan spiritual masyarakat pesisir yang telah berdiri sejak masa lampau dan mengalami berbagai pemugaran.",
-  //   yearEstablished: "~1930",
-  //   significanceLevel: 4,
-  //   imageUrl: "/assets/images/masjid.jpg",
-  // },
   {
     id: "site-06",
     name: "Pantai Kuala Dewi",
@@ -125,39 +92,28 @@ export const historicalSites = [
       "Pantai Kuala Dewi berdiri tahun 2004, dikelola oleh Kelompok Tunas Baru. Awalnya laut, lalu ditimbun bertahap hingga menjadi pantai.",
     yearEstablished: "~2004",
     significanceLevel: 4,
-    imageUrl: "/assets/images/pantai-kuala-dewi.jpg", // Diperbaiki dari masjid.jpg
+    imageUrl: "/assets/images/pantai-kuala-dewi.jpg",
   },
-  // {
-  //   id: "site-07",
-  //   name: "Tugu Peringatan / Monumen Desa",
-  //   category: "sejarah",
-  //   coordinates: [3.65123, 98.96145],
-  //   shortDescription:
-  //     "Marka sejarah yang menjadi simbol perkembangan kawasan pesisir Kota Pari dari desa nelayan tradisional menjadi area terpadu.",
-  //   yearEstablished: "1980-an",
-  //   significanceLevel: 4,
-  //   imageUrl: "/assets/images/tugu-desa.jpg",
-  // },
-  // {
-  //   id: "site-08",
-  //   name: "Situs Menara Pantau Nelayan",
-  //   category: "maritim",
-  //   coordinates: [3.66785, 98.95562],
-  //   shortDescription:
-  //     "Titik pandang lawas yang dahulu sering digunakan oleh nelayan lokal untuk memantau pasang surut air laut dan navigasi kapal secara tradisional.",
-  //   yearEstablished: "1970-an",
-  //   significanceLevel: 3,
-  //   imageUrl: "/assets/images/menara-pantau.jpg",
-  // },
   {
     id: "site-09",
     name: "Woong Rame Pantai Cermin",
-    category: "Wisata",
+    category: "wisata",
     coordinates: [3.6583582317405545, 98.9703996888711],
     shortDescription:
       "Titik pandang lawas yang dahulu sering digunakan oleh nelayan lokal untuk memantau pasang surut air laut dan navigasi kapal secara tradisional.",
     yearEstablished: "~ 2004",
     significanceLevel: 4,
     imageUrl: "/assets/images/galeri-1.png",
+  },
+  {
+    id: "site-10",
+    name: "Rumah Datuk Pengembara",
+    category: "sejarah",
+    coordinates: [3.653283, 98.972858],
+    shortDescription:
+      "Sejak tahun 2006, festival ini rutin diselenggarakan pada akhir tahun. Berawal dari visi Iswanto Browo (seorang tokoh Tionghoa) mengenai persinggahan Datuk beragama Islam di Desa Kota Pari.",
+    yearEstablished: "~ 2013",
+    significanceLevel: 4,
+    imageUrl: "/assets/images/ceritalegenda/datuk_pengembara.jpeg",
   },
 ];
